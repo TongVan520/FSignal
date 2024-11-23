ce8b6718c0d7c0676ce2ab15cc20c1924e0e0bed
tvFireFlower <3408582386@qq.com>
2024-11-23 11:32:39 +0800

    Initial commit

---
+++ src/FSignal/FSignal.cpp
@@ -0,0 +1,9 @@
+//
+// Created by FireFlower on 2024/11/23.
+//
+
+#include "FSignal.h"
+
+namespace fireflower {
+
+}
---
+++ src/FSignal/FSignal.h
@@ -0,0 +1,16 @@
+//
+// Created by FireFlower on 2024/11/23.
+//
+
+#ifndef FSIGNAL_FSIGNAL_H
+#define FSIGNAL_FSIGNAL_H
+
+namespace fireflower {
+	/// @类名 信号
+	/// @描述 线程安全
+	class FSignal {
+	
+	};
+}
+
+#endif //FSIGNAL_FSIGNAL_H
